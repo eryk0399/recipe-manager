e5f6235cb61d357c6540d9289c68fcafb0bc7adf
eryk0399 <eryk0399@icloud.com>
2025-01-29 18:30:28 +0100

    start working on recipe form

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,19 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Form\RecipeFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\RecipeRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 final class RecipeController extends AbstractController
 {
-    #[Route('/recipe', name: 'app_recipe')]
-    public function index(RecipeRepository $repository): Response
+    public function __construct(private RecipeRepository $recipeRepository) {}
+
+    #[Route('/recipes', name: 'recipes')]
+    public function index(): Response
     {
-        $recipes = $repository->findAll();
+        $recipes = $this->recipeRepository->findAll();
         return $this->render('recipe/index.html.twig', [
             'recipes' => $recipes,
+        ]);
+    }
+
+    #[Route('/recipes/create', name: 'recipes_create')]
+    public function create(Request $request): Response 
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeFormType::class, $recipe);
+        $recipe->setInstructions(['instruction 1', 'instruction 2']);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+        }
+
+        return $this->render('recipe/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/recipes/{id}', methods: ['GET'], name: 'recipes_show')]
+    public function show($id): Response 
+    {
+        return $this->render('recipe/show.html.twig', [
+            'recipe' => $this->recipeRepository->find($id)
         ]);
     }
 }
